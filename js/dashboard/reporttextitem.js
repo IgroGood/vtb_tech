bd8cb36cef39ef409f2be80bc3c76650215e8efd
IgroGood <IgroGood@mail.ru>
2021-10-10 07:35:33 +1200

    fixbugs

--- js/dashboard/reporttextitem.js
+++ js/dashboard/reporttextitem.js
@@ -1,11 +1,8 @@
-function createTextItem()
-{
+function createTextItem(){
     var text = document.createElement("div");
-
     text.className = "ReportItem";
     text.innerHTML = "Text";
     text.setAttribute("data-reportitemtype", "ReportTextItem");
     text.setAttribute("contenteditable", "true");
-
     return text;
 }
